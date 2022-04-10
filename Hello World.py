606e780b9598ab725f175baae1bd310751902660
aldinrd29 <102130312+aldinrd29@users.noreply.github.com>
2022-04-10 23:28:51 +0700

    Menampilkan teks menggunakan phyton

---
+++ Hello World.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+print("Hello World")
+print("Saya senang sekali belajar Phyton")
+print("Saya Aldi Nurudin Mahasiswa Teknik Komputer Universitas Wiralodra!")
+
+
+# In[ ]:
+
+
+
+
